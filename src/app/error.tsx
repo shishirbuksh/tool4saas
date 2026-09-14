aef52423896e34fe2b1b99ab195469b185ed7827
Shishir Buksh <shishirbuksh97@gmail.com>
2026-09-14 16:26:21 +0530

    fix: Ignore TS error for LinkWrapper dynamic href

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -10,6 +10,7 @@ import React from "react";
 import Link from "next/link";
 
 const LinkWrapper = React.forwardRef<HTMLAnchorElement, any>((props, ref) => (
+  // @ts-expect-error - MUI passes href dynamically
   <Link ref={ref} {...props} />
 ));
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import Link from "next/link";
 
 const LinkWrapper = React.forwardRef<HTMLAnchorElement, any>((props, ref) => (
+  // @ts-expect-error - MUI passes href dynamically
   <Link ref={ref} {...props} />
 ));
 import { usePathname } from "next/navigation";
